75eff46fcaf9f542caccc64e7f3134d4a7daf71a
nati <nsisay49@gmail.com>
2025-08-02 07:19:07 -0600

    prisma installation

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
-import itemRoutes from "./routes/itemRoutes";
+// import itemRoutes from "./routes/itemRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
+import config from "./config/config";
 
 const app = express();
 
 app.use(express.json());
 
-// Routes
-app.use("/api/items", itemRoutes);
+// // Routes
+// app.use("/api/items", itemRoutes);
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
+
+app.listen(config.port, () => console.log("hello to the back end!"));
 
 export default app;
